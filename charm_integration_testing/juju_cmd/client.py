af8daddf3825c49661d0c4dab344788ec1d6d05f
Alex Balderson <alexander.balderson@canonical.com>
2025-01-16 10:00:03 -0700

    format

--- charm_integration_testing/juju_cmd/client.py
+++ charm_integration_testing/juju_cmd/client.py
@@ -33,15 +33,11 @@ class JujuCmdClient(JujuClient):
 
     def scale_application(self, application: str, num: int):
         # Get current juju units
-        units = sorted(self._status().applications[application].units.keys(), key=lambda unit: unit.split("/", 1)[1])
+        # units = sorted(self._status().applications[application].units.keys(), key=lambda unit: unit.split("/", 1)[1])
 
         # Add or remove units
         # juju scale-application does not work with VM charms XXX: SQT-431
-        self._call_juju(
-            CmdArg(value="scale-application"),
-            CmdArg(value=application),
-            CmdArg(value=num)
-        )
+        self._call_juju(CmdArg(value="scale-application"), CmdArg(value=application), CmdArg(value=num))
         # if len(units) < num:
         #     self._call_juju(
         #         CmdArg(value="add-unit"),
