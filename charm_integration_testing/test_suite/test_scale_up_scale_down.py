a954d284611ef5c16f6003e60f7f114c513f7d98
Alex Balderson <alexander.balderson@canonical.com>
2025-01-21 13:03:58 -0700

    Revert "got its a friday"
    This reverts commit 26e80af5ebd57163b86ce6c71f2ca4edba7dd2c2.

--- charm_integration_testing/test_suite/test_scale_up_scale_down.py
+++ charm_integration_testing/test_suite/test_scale_up_scale_down.py
@@ -11,6 +11,9 @@ from charm_integration_testing.juju import JujuClient
 
 @pytest.mark.timeout(timedelta(minutes=15).total_seconds())
 def test_scale_down_scale_up_charm(juju_client: JujuClient, model: str, requirer_application: str):
+    # Ensure ready
+    juju_client.idle_for_period(model=model)
+
     # Get units
     num_units = juju_client.num_units(requirer_application)
 
@@ -20,4 +23,4 @@ def test_scale_down_scale_up_charm(juju_client: JujuClient, model: str, requirer
 
     # Rescale application
     juju_client.scale_application(requirer_application, num_units)
-    juju_client.idle_for_period(model=model)
+    juju_client.idl_for_period(model=model)
