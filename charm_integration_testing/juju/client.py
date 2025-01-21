a51e88a99616a672fca7dccabed45f36cb79829b
Ryan Britton <ryan.britton@canonical.com>
2025-01-21 17:33:07 +0000

    Add additional logs

--- charm_integration_testing/juju/client.py
+++ charm_integration_testing/juju/client.py
@@ -24,36 +24,48 @@ class JujuClient:
         self.logger.info(f"Getting the number of units for {application}.")
         return self.backend.num_units(application)
 
-    # Logging wrapper around wait idle
-    def wait_idle(self, model: str = "default", timeout: timedelta = timedelta(days=1), idle_period: timedelta = timedelta(seconds=15)):
+    # Wait for the Juju model to become idle
+    def idle_for_period(self, model: str = "default", timeout: timedelta = timedelta(days=1), idle_period: timedelta = timedelta(seconds=15)):
         # Start logging
         self.logger.info("Waiting for idle.\n::group::Juju Status:")
 
         # Wait for idle
         try:
-            # Loop until timeout
-            end_time = datetime.now(timezone.utc) + timeout
-            idle_since = None
-            while datetime.now(timezone.utc) < end_time:
-                try:
-                    self.backend.wait_idle(model=model, timeout=timedelta(seconds=5))
-                except JujuWaitIdleTimeoutError:
-                    # Wait idle did not conclude, try again
-                    idle_since = None
-                else:
-                    # Applications are idle
-                    if idle_since is None:
-                        idle_since = datetime.now(timezone.utc)
-                    elif datetime.now(timezone.utc) - idle_since > idle_period:
-                        return
-                    time.sleep(1)
-                finally:
-                    # Always print status
-                    self.print_status()
+            self._idle_for_period(model, timeout, idle_period)
         finally:
             # Always end group log
             self.logger.info("End waiting for idle.\n::endgroup::")
-
+    
+    # Ensure the Juju model is idle for the given period
+    def _idle_for_period(self, model: str, timeout: timedelta, idle_period: timedelta):
+        # Loop until timeout
+        start_time = datetime.now(timezone.utc)
+        idle_since = None
+        while datetime.now(timezone.utc) < start_time + timeout:
+            try:
+                self.backend.wait_idle(model=model, timeout=timedelta(seconds=1))
+            except JujuWaitIdleTimeoutError:
+                # Model not idle, try again
+                idle_since = None
+                self.print_status()
+            else:
+                # Model is idle
+                if idle_since is None:
+                    # Model is idle for the first time
+                    self.print_status()
+                    self.logger.info("Applications are idle, checking for idle period.")
+                    idle_since = datetime.now(timezone.utc)
+                elif datetime.now(timezone.utc) - idle_since > idle_period:
+                    # Model is still idle and idle_period is met
+                    self.logger.info("Applications are now idle.")
+                    return
+                else:
+                    # Model is still idle but idle_period not met
+                    self.logger.info("Applications are still idle.")
+                
+                # Wait before checking again
+                time.sleep(1)
+        
         raise JujuWaitIdleTimeoutError
 
     def print_status(self):
