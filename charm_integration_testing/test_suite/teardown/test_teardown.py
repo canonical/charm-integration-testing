09991cdacfca6ff25b996154e785cc28922e8f3e
Ryan Britton <ryan.britton@canonical.com>
2025-02-14 12:47:52 -0500

    Check for idle and existence before teardown (#28)

--- charm_integration_testing/test_suite/teardown/test_teardown.py
+++ charm_integration_testing/test_suite/teardown/test_teardown.py
@@ -10,10 +10,6 @@ from juju import JujuClient
 
 @pytest.mark.timeout(timedelta(minutes=15).total_seconds())
 def test_teardown(juju_client: JujuClient, model: str, applications: list[str]):
-    # Ensure applications exist
-    for application in applications:
-        assert juju_client.application_exists(application, model=model)
-
     # Remove all requested applications
     juju_client.remove_applications(*applications, model=model)
 
