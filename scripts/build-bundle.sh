6ded7aa47c683b62adebe9bf58140fec256e85ce
Raul Almeida <raul.almeida@canonical.com>
2025-09-24 15:27:46 -0300

    ci: add static charm priorities file and respective usage in build-bundle.sh

--- scripts/build-bundle.sh
+++ scripts/build-bundle.sh
@@ -8,4 +8,5 @@ poetry run bundle-builder \
     --charm-platform-overrides ./static/charm-platform-overrides/ \
     --charm-listing-overrides ./static/charm-listing-overrides.yaml \
     --charm-test-configs ./static/charm-test-configs/ \
+    --charm-priorities ./static/charm-priorities.yaml \
     "$@"
