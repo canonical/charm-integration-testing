321aed16b331ee7c1ed17609c13868a13496b532
Ryan Britton <ryan.britton@canonical.com>
2025-10-28 18:41:30 +0000

    Clarify deploy error

--- charm_integration_testing/juju_cmd/backend.py
+++ charm_integration_testing/juju_cmd/backend.py
@@ -2,6 +2,7 @@
 # See LICENSE file for licensing details.
 
 
+import os
 import time
 from datetime import datetime, timedelta, timezone
 
@@ -219,6 +220,8 @@ class JujuCmdBackend(JujuBackend):
         )
 
     def deploy_bundle_file(self, model: str, bundle: str):
+        if not os.path.isfile(bundle):
+            raise ValueError("Bundle file not found.")
         self._call_juju(
             CmdArg(value="deploy"),
             CmdArg(name="model", value=model),
