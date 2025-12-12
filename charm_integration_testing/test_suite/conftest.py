016e6de1df6597d17e4e02611d3128e557a329be
Ryan Britton <ryan.britton@canonical.com>
2025-12-12 17:35:51 +0000

    Trim token

--- charm_integration_testing/test_suite/conftest.py
+++ charm_integration_testing/test_suite/conftest.py
@@ -76,13 +76,17 @@ def model(request: pytest.FixtureRequest) -> str:
 @pytest.fixture
 def minio_client_file() -> Path | None:
     file_path = os.environ.get(MINIO_CLIENT_FILE_ENV)
-    return Path(file_path) if file_path and file_path.strip() else None
+    if file_path:
+        file_path = file_path.strip()
+    return Path(file_path) if file_path else None
 
 
 @pytest.fixture
 def ubuntu_pro_token() -> str | None:
     token = os.environ.get(UBUNTU_PRO_TOKEN_ENV)
-    return token if token and token.strip() else None
+    if token:
+        token = token.strip()
+    return token if token else None
 
 
 failure_message = StashKey[CollectReport]()
