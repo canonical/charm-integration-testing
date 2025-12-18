2452ea4024153381b1a93c295c6717002ee4504b
Raul Almeida <raul.almeida@canonical.com>
2025-12-18 10:31:02 -0300

    chore: annotate immutable_dataclass.py

--- bundle_builder/bundle_builder/immutable_dataclass.py
+++ bundle_builder/bundle_builder/immutable_dataclass.py
@@ -16,19 +16,29 @@
 
 from dataclasses import field
 from functools import wraps
-from typing import Any, Callable, Dict, get_type_hints
+from typing import Any, Callable, Dict, TypeVar, get_type_hints
 
 from pydantic.dataclasses import dataclass
+from typing_extensions import dataclass_transform
+
+_F = TypeVar("_F", bound=Callable[..., Any])
+
+
+# Custom property subclass that allows marking for computed properties
+class _ComputedProperty(property):
+    _is_computed_property = True
+
+    def __init__(self, func: Callable[..., Any]) -> None:
+        super().__init__(func)
+        self._original_func = func
 
 
 # Computed property attribute
 # Meant for use with @immutable_dataclass
 # Computed at initialization once
-# TODO(raul): remove type ignore in subsequent type checker PRs
-def computed_property(func: Callable) -> Callable:  # type: ignore
-    # TODO(raul): remove type ignore in subsequent type checker PRs
-    func._is_computed_property = True  # type: ignore
-    return func
+def computed_property(func: _F) -> Any:
+    # Return a custom property that mypy understands as a property
+    return _ComputedProperty(func)
 
 
 # Sentinel value for uninitialized computed fields
@@ -36,10 +46,8 @@ _UNINITIALIZED = object()
 
 
 # Create a lazy property that computes its value once
-# TODO(raul): remove type ignore in subsequent type checker PRs
-def make_lazy_property(private_name, method):  # type: ignore
-    # TODO(raul): remove type ignore in subsequent type checker PRs
-    def prop(self):  # type: ignore
+def make_lazy_property(private_name: str, method: Callable[..., Any]) -> Any:
+    def prop(self: object) -> Any:
         value = getattr(self, private_name)
         if value is _UNINITIALIZED:
             value = method(self)
@@ -59,18 +67,15 @@ _CACHE_MISS = object()
 # Cached method backed by instance-level cache
 # Meant for use with @immutable_dataclass
 # Much like functools.cache, but at the instance level instead of global
-# TODO(raul): remove type ignore in subsequent type checker PRs
-def cached_method(func):  # type: ignore
+def cached_method(func: _F) -> _F:
     setattr(func, _MARKED_AS_CACHED_METHOD, True)
     return func
 
 
 # Wraps the method to cache results in the given field in the instance
-# TODO(raul): remove type ignore in subsequent type checker PRs
-def make_cached_method(cached_field_name, method):  # type: ignore
+def make_cached_method(cached_field_name: str, method: Callable[..., Any]) -> Callable[..., Any]:
     @wraps(method)
-    # TODO(raul): remove type ignore in subsequent type checker PRs
-    def wrapped(*args, **kwargs):  # type: ignore
+    def wrapped(*args: str, **kwargs: Any) -> Any:
         cache = getattr(args[0], cached_field_name)
         cache_key = tuple(args[1:]) + tuple(kwargs.items())
         result = cache.get(cache_key, _CACHE_MISS)
@@ -84,16 +89,17 @@ def make_cached_method(cached_field_name, method):  # type: ignore
 
 # Create an immutable dataclass using frozen=True
 # and defaults slots=True
-# TODO(raul): remove type ignore in subsequent type checker PRs
-def immutable_dataclass(_cls=None, **dataclass_kwargs):  # type: ignore
-    # TODO(raul): remove type ignore in subsequent type checker PRs
-    def wrap(cls):  # type: ignore
+@dataclass_transform(frozen_default=True, field_specifiers=(field,))
+def immutable_dataclass(_cls: type | None = None, **dataclass_kwargs: Any) -> Any:
+    def wrap(cls: type) -> Any:
         # Collect methods decorated as computed fields
         computed_fields = {}
         cached_methods = {}
         for name, val in cls.__dict__.items():
             if getattr(val, "_is_computed_property", False):
-                computed_fields[name] = val
+                # Extract the original function from the property
+                original_func = getattr(val, "_original_func", val.fget if isinstance(val, property) else val)
+                computed_fields[name] = original_func
             elif getattr(val, _MARKED_AS_CACHED_METHOD, False):
                 cached_methods[name] = val
 
@@ -103,17 +109,14 @@ def immutable_dataclass(_cls=None, **dataclass_kwargs):  # type: ignore
             # Modify the class in place to add the private field
             private_name = f"_{name}"
             annotations[private_name] = get_type_hints(method).get("return", Any)
-            # TODO(raul): remove type ignore in subsequent type checker PRs
-            setattr(cls, name, make_lazy_property(private_name, method))  # type: ignore
+            setattr(cls, name, make_lazy_property(private_name, method))  # type: ignore[arg-type]
             setattr(cls, private_name, field(init=False, repr=False, hash=False, compare=False, default=_UNINITIALIZED))
 
         # Create a private slot for the cache of each computed field, similar to above
         for name, method in cached_methods.items():
             cached_field_name = f"_cached_{name}"
-            # TODO(raul): remove type ignore in subsequent type checker PRs
-            annotations[cached_field_name] = Dict[tuple, Any]  # type: ignore
-            # TODO(raul): remove type ignore in subsequent type checker PRs
-            setattr(cls, name, make_cached_method(cached_field_name, method))  # type: ignore
+            annotations[cached_field_name] = Dict[tuple[Any, ...], Any]
+            setattr(cls, name, make_cached_method(cached_field_name, method))
             setattr(
                 cls, cached_field_name, field(init=False, repr=False, hash=False, compare=False, default_factory=dict)
             )
@@ -126,7 +129,6 @@ def immutable_dataclass(_cls=None, **dataclass_kwargs):  # type: ignore
 
     # Handle both @decorator and @decorator()
     if _cls is not None and isinstance(_cls, type):
-        # TODO(raul): remove type ignore in subsequent type checker PRs
-        return wrap(_cls)  # type: ignore
+        return wrap(_cls)
 
     return wrap
