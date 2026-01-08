87e1010e80d833308ea66b29b74ed106c9e2353c
Moises E. Benzan M. <moises.benzan@canonical.com>
2026-01-08 09:58:32 -0400

    suggestions

--- charm_integration_testing/test_suite/conftest.py
+++ charm_integration_testing/test_suite/conftest.py
@@ -314,32 +314,31 @@ def record_pipeline_version_execution_metadata(
     execution_metadata: Callable[[str, str | int], None],
     request: pytest.FixtureRequest,
     logger: logging.Logger,
-):
+) -> None:
     pipeline_path: Path = Path(request.config.rootpath) / ".github" / "workflows" / "charm-testing.yaml"
 
-    if not pipeline_path.exists():
+    if pipeline_path.exists():
+        try:
+            repository_version_command = ["git", "--no-pager", "log", "-n", "1", "--pretty=format:%h"]
+            pipeline_version_command = [
+                "git",
+                "hash-object",
+                "--",
+                str(pipeline_path.resolve()),
+            ]
+            logger.info(f"Executed command: {pipeline_version_command}")
+
+            repository_result = run(repository_version_command, capture_output=True, text=True, check=True)  # nosec B603
+            pipeline_result = run(pipeline_version_command, capture_output=True, text=True, check=True)  # nosec B603
+
+            # Add version metadata before test
+            execution_metadata("versions:pipeline_git_hash", pipeline_result.stdout.strip())
+            execution_metadata("versions:repository_version", repository_result.stdout.strip())
+
+        except CalledProcessError as e:
+            execution_metadata("warning:message", normalize_string(f"Failed to get version information from git: {e}."))
+
+    else:
         execution_metadata("warning:message", normalize_string(f"Pipeline file not found: {pipeline_path}"))
-        yield
-        return
-
-    try:
-        repository_version_command = ["git", "--no-pager", "log", "-n", "1", "--pretty=format:%h"]
-        pipeline_version_command = [
-            "git",
-            "hash-object",
-            "--",
-            str(pipeline_path.resolve()),
-        ]
-        logger.info(f"Executed command: {pipeline_version_command}")
-
-        repository_result = run(repository_version_command, capture_output=True, text=True, check=True)  # nosec B603
-        pipeline_result = run(pipeline_version_command, capture_output=True, text=True, check=True)  # nosec B603
-
-        # Add version metadata before test
-        execution_metadata("versions:pipeline_git_hash", pipeline_result.stdout.strip())
-        execution_metadata("versions:repository_version", repository_result.stdout.strip())
-
-    except CalledProcessError as e:
-        execution_metadata("warning:message", normalize_string(f"Failed to get version information from git: {e}."))
 
     yield
