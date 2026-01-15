96a19ff86550aedb435a79da2dace64ed255c436
Peishuo Cai <98190011+PeishuoCai@users.noreply.github.com>
2026-01-15 11:25:45 -0500

    [TS-43] Add yaml formatter (#293)
    * adding yamllint lint check for all yaml and yml files in the repo
    
    * adding yamlfix as the yaml formatter before the lint check
    
    * applying yamlfix to previously existing yaml files in repo
    
    * removing formatter from lint.sh and adding to format.sh
    
    * updating linter to use yamlfix instead of yamllint, upgrading and applying fixes from newest release of yamlfix

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,4 +5,5 @@
 cd "$(dirname "$0")/.."
 poetry run ruff check --fix
 poetry run ruff format
+poetry run yamlfix $(find . -type f \( -name "*.yaml" -o -name "*.yml" \))
 markdownlint-cli2 --fix --config docs/.sphinx/.markdownlint.json "#docs/_build" "*.md"
