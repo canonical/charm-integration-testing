cf7566ef2a68eeff96777f20fa0a6d692ecafdea
Ryan Britton <ryan.britton@canonical.com>
2026-01-15 19:17:43 +0000

    Revert "[TS-43] Add yaml formatter (#293)"
    This reverts commit 96a19ff86550aedb435a79da2dace64ed255c436.

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,5 +5,4 @@
 cd "$(dirname "$0")/.."
 poetry run ruff check --fix
 poetry run ruff format
-poetry run yamlfix $(find . -type f \( -name "*.yaml" -o -name "*.yml" \))
 markdownlint-cli2 --fix --config docs/.sphinx/.markdownlint.json "#docs/_build" "*.md"
