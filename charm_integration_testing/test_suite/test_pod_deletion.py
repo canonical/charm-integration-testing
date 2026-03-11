7f6bc1e98ca92dee335a8c562ebfbac5ac2360ea
amc94 <amjad.chami@canonical.com>
2026-03-11 13:40:44 -0400

    formatting

--- charm_integration_testing/test_suite/test_pod_deletion.py
+++ charm_integration_testing/test_suite/test_pod_deletion.py
@@ -30,7 +30,7 @@ def test_pod_deletion(juju_client: JujuClient, model: str, target_application: s
 
     if not juju_client.backend.is_k8s_model(model):
         pytest.skip("This test is only applicable for Kubernetes models.")
-        
+
     try:
         k8s_config.load_kube_config()
     except k8s_config.ConfigException:
