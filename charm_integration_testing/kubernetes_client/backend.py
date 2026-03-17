75d45b20aa3e88e60587ae43c8dcc92f960ec308
amc94 <amjad.chami@canonical.com>
2026-03-17 07:34:02 -0400

    use the correct property

--- charm_integration_testing/kubernetes_client/backend.py
+++ charm_integration_testing/kubernetes_client/backend.py
@@ -49,7 +49,7 @@ class KubernetesBackend:
             List of pods that match the given charm name in the specified namespace
         """
         pods = self.client.list_namespaced_pod(model)  # juju creates a namespace for each model
-        matching_pods = [pod for pod in pods if pod.metadata.name.startswith(charm)]
+        matching_pods = [pod for pod in pods.items if pod.metadata.name.startswith(charm)]
         return matching_pods
 
     def wait(
