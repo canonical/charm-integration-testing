85ddf1633bdae7570c6141d856a8e20c1b82c747
Moises Benzan <moises.benzan@canonical.com>
2026-03-24 10:06:04 -0400

    unexposes private property

--- charm_integration_testing/juju/backend.py
+++ charm_integration_testing/juju/backend.py
@@ -9,7 +9,6 @@ from datetime import datetime, timedelta
 from functools import wraps
 from typing import Any, ParamSpec, TypeVar
 
-from kubernetes_client.client import KubernetesClient
 from pydantic.dataclasses import dataclass
 
 from validators.base.validator import ValidationResult
@@ -139,11 +138,6 @@ class JujuTask:
 
 
 class JujuBackend(ABC):
-    @property
-    @abstractmethod
-    def kubernetes_client(self) -> KubernetesClient:
-        raise NotImplementedError
-
     @abstractmethod
     def scale_application(self, model: str, application: str, num: int) -> None:
         raise NotImplementedError
