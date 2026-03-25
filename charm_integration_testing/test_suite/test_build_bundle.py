6a9985be7ebadffb25470892fcc9c1a17f95f843
Moises Benzan <moises.benzan@canonical.com>
2026-03-25 14:03:35 -0400

    suggestions

--- charm_integration_testing/test_suite/test_build_bundle.py
+++ charm_integration_testing/test_suite/test_build_bundle.py
@@ -85,11 +85,11 @@ def test_build_bundle(
     bundle_builder = BundleBuilder(charmhub_client=charmhub_client, logger=logger)
     built_bundle = bundle_builder.build(base_bundle)
 
-    bundle_contents = built_bundle.export().strip()
+    bundle_contents = built_bundle.export()
     separator = "-" * 80
     bundle.write_text(bundle_contents, encoding="utf-8")
     logger.info(f"Bundle written to {bundle}.")
-    logger.info(f"Bundle content:\n{separator}\n{bundle_contents}\n{separator}")
+    logger.info(f"Bundle content:\n{separator}\n{bundle_contents.strip()}\n{separator}")
 
     bundle_mermaid_output.write_text(built_bundle.export_mermaid(), encoding="utf-8")
     logger.info(f"Bundle Mermaid diagram written to {bundle_mermaid_output}")
