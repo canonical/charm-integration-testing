69f2906c9050c7b61fd99eb45b83922e23526982
amc94 <amjad.chami@canonical.com>
2026-05-01 03:24:38 -0400

    Revert "remove core marker"
    This reverts commit 6259c90a99fae48e191c021177bc3fa0261f02d1.

--- charm_integration_testing/test_suite/test_create_model.py
+++ charm_integration_testing/test_suite/test_create_model.py
@@ -7,6 +7,7 @@ from juju import JujuClient
 from .scheduler.states import State
 
 
+@pytest.mark.core
 @pytest.mark.state(requires=State.NO_MODEL, provides=State.EMPTY_MODEL, bridge_only=True)
 def test_create_model(
     juju_client: JujuClient, juju_controller: str, model: str, juju_model_config: dict[str, str]
