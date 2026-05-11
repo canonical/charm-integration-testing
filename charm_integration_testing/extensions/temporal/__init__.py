b4f942b729e9493d02f9ea781ed6d27423510ad7
Ryan Britton <ryan.britton@canonical.com>
2026-05-11 12:32:02 +0000

    Update temporal deployment configuration

--- charm_integration_testing/extensions/temporal/__init__.py
+++ charm_integration_testing/extensions/temporal/__init__.py
@@ -1,6 +1,16 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import warnings
+
 from .extension import TemporalExtension
+
+warnings.warn(
+    "charm_integration_testing.extensions.temporal is deprecated and will be removed in a future release. "
+    "temporal-worker-k8s now connects to Temporal via an explicit relation; "
+    "TemporalExtension is no longer needed.",
+    DeprecationWarning,
+    stacklevel=2,
+)
 
 __all__ = ["TemporalExtension"]
