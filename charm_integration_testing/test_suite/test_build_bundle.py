ebe4cbbbb5f53864d0ba485097ad64d4a4caf0a8
amc94 <amjad.chami@canonical.com>
2026-05-21 05:23:42 -0400

    trying out testmon

--- charm_integration_testing/test_suite/test_build_bundle.py
+++ charm_integration_testing/test_suite/test_build_bundle.py
@@ -21,7 +21,6 @@ from bundle_builder import (
 from .scheduler.states import State
 
 
-@pytest.mark.core
 @pytest.mark.state(requires=State.NO_BUNDLE, provides=State.NO_CONTROLLER)
 def test_build_bundle(
     target_charm: str,
